ed0147baa174c19550055357de639de0d575aa21
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2018-09-06 02:33:44 +0900

    feat:채팅 메세지 전송 기능 완료

--- app/src/main/java/endorphine/icampyou/ChattingMessage_Fragment.java
+++ app/src/main/java/endorphine/icampyou/ChattingMessage_Fragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class ChattingMessage_Fragment extends Fragment {
@@ -20,16 +22,34 @@ public class ChattingMessage_Fragment extends Fragment {
 
         m_chatMessage_listView.setAdapter(m_chatmessage_adapter);
 
-        m_chatmessage_adapter.add("이건 뭐지",1);
-        m_chatmessage_adapter.add("쿨쿨",1);
-        m_chatmessage_adapter.add("쿨쿨쿨쿨",0);
-        m_chatmessage_adapter.add("재미있게",1);
-        m_chatmessage_adapter.add("놀자라구나힐힐 감사합니다. 동해물과 백두산이 마르고 닳도록 놀자 놀자 우리 놀자",1);
-        m_chatmessage_adapter.add("재미있게",1);
-        m_chatmessage_adapter.add("재미있게",0);
-        m_chatmessage_adapter.add("2015/11/20",2);
-        m_chatmessage_adapter.add("재미있게",1);
-        m_chatmessage_adapter.add("재미있게",1);
+        final EditText send_message = (EditText)view.findViewById(R.id.editText1);
+
+        view.findViewById(R.id.send_btn).setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(send_message.length() == 0){
+                    return ;
+                }
+                else{
+                    m_chatmessage_adapter.add(send_message.getText().toString(),1);
+                    send_message.setText("");
+                    m_chatmessage_adapter.notifyDataSetChanged();
+                }
+
+            }
+        });
+
+//
+//        m_chatmessage_adapter.add("이건 뭐지",1);
+//        m_chatmessage_adapter.add("쿨쿨",1);
+//        m_chatmessage_adapter.add("쿨쿨쿨쿨",0);
+//        m_chatmessage_adapter.add("재미있게",1);
+//        m_chatmessage_adapter.add("놀자라구나힐힐 감사합니다. 동해물과 백두산이 마르고 닳도록 놀자 놀자 우리 놀자",1);
+//        m_chatmessage_adapter.add("재미있게",1);
+//        m_chatmessage_adapter.add("재미있게",0);
+//        m_chatmessage_adapter.add("2015/11/20",2);
+//        m_chatmessage_adapter.add("재미있게",1);
+//        m_chatmessage_adapter.add("재미있게",1);
 
         return view;
     }
