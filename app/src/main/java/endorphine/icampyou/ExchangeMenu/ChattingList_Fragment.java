2fb247b5e6b468dd4c0b6f55b804d9660cbc751a
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2018-09-12 03:44:40 +0900

    feat: 채팅 목록 오류 수정 및 검색 기능 구현

--- app/src/main/java/endorphine/icampyou/ExchangeMenu/ChattingList_Fragment.java
+++ app/src/main/java/endorphine/icampyou/ExchangeMenu/ChattingList_Fragment.java
@@ -11,16 +11,21 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 import endorphine.icampyou.BaseFragment;
 import endorphine.icampyou.R;
@@ -31,8 +36,11 @@ public class ChattingList_Fragment extends BaseFragment {
 
     final int save_info = 1;
 
-    ListView listView = null;
+    ArrayList<Chat_Item> copy;
+    ArrayList<Chat_Item> items;
     ChatList_Adapter adapter;
+    ListView chatlist_listView;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -60,6 +68,7 @@ public class ChattingList_Fragment extends BaseFragment {
         ts2.setIndicator("나의 채팅");
         tabHost1.addTab(ts2);
 
+        //채팅방 목록 생성하는 버튼
         Button add_chatlist_btn = (Button) view.findViewById(R.id.make_chatlist_button);
         add_chatlist_btn.setOnClickListener(new View.OnClickListener()
         {
@@ -71,59 +80,66 @@ public class ChattingList_Fragment extends BaseFragment {
             }
         });
 
-        adapter = new ChatList_Adapter(getActivity());
+        items = new ArrayList<>();
+        copy = new ArrayList<>();
+        items.add(new Chat_Item(null,"1","1","1","1"));
+        items.add(new Chat_Item(null,"2","2","2","2"));
+        items.add(new Chat_Item(null,"3","3","3","3"));
+        items.add(new Chat_Item(null,"4","4","4","4"));
 
-        //listView = (ListView) view.findViewById(R.id.camp_chat_listview);
+        copy.addAll(items);
+        chatlist_listView =(ListView) view.findViewById(R.id.camp_chat_listview);
 
-        adapter.add(null,"전세영","냥냥","냥냥");
+        adapter = new ChatList_Adapter(items);
 
-        //listView.setAdapter(adapter);
+        chatlist_listView.setAdapter(adapter);
 
+        //채팅방 들어가기
+        chatlist_listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view ,int position,long id){
+                startFragment(getFragmentManager(),ChattingMessage_Fragment.class);
 
-//        //채팅방 들어가기
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view ,int position,long id){
-//                startFragment(getFragmentManager(),ChattingMessage_Fragment.class);
-//            }
-//        });
-//
-//        //채팅 목록 삭제
-//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-//            @Override
-//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int position, long arg3) {
-//
-//                DialogInterface.OnClickListener positiveListener = new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        adapter.remove(position);
-//                        listView.setAdapter(adapter);
-//                    }
-//                };
-//
-//                DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        dialog.dismiss();
-//                    }
-//                };
-//
-//                new AlertDialog.Builder(getActivity())
-//                        .setTitle("채팅 목록을 삭제하시겠습니까?")
-//                        .setPositiveButton("예",positiveListener)
-//                        .setNegativeButton("취소",cancelListener).show();
-//
-//                return true;
-//            }
-//        });
+            }
+        });
+
+        //채팅 목록 삭제
+        chatlist_listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(final AdapterView<?> adapterView, View view, final int position, long arg3) {
+
+                DialogInterface.OnClickListener positiveListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.e("position",String.valueOf(position));
+                        items.remove(position);
+                        chatlist_listView.setAdapter(adapter);
+                    }
+                };
+
+                DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                };
+
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("채팅 목록을 삭제하시겠습니까?")
+                        .setPositiveButton("예",positiveListener)
+                        .setNegativeButton("취소",cancelListener).show();
+
+                return true;
+            }
+        });
 
         editSearch = (EditText)view.findViewById(R.id.search);
         editSearch.addTextChangedListener(new TextWatcher(){
 
             @Override
             public void afterTextChanged(Editable edit) {
-                //String text = editSearch.getText().toString().toLowerCase(Locale.getDefault());
-                //adapter.filter(text);
+                String text = editSearch.getText().toString().toLowerCase(Locale.getDefault());
+                search(text);
             }
 
             @Override
@@ -142,12 +158,42 @@ public class ChattingList_Fragment extends BaseFragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);
+        Log.e("requestCode",String.valueOf(requestCode));
+        Log.e("resultCode",String.valueOf(resultCode));
         String pass_user = data.getStringExtra("user");
         String pass_need =data.getStringExtra("need");
         String pass_lettable = data.getStringExtra("lettable");
         byte[] image_byte = data.getByteArrayExtra("image");
         Bitmap pass_image = BitmapFactory.decodeByteArray(image_byte,0,image_byte.length);
-        adapter.add(pass_image,pass_user,pass_need,pass_lettable);
-        listView.setAdapter(adapter);
+        adapter.add(pass_image,pass_user,pass_need,pass_lettable,"난징");
+        chatlist_listView.setAdapter(adapter);
+    }
+
+    // 검색을 수행하는 메소드
+    public void search(String charText) {
+
+        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
+        items.clear();
+
+        // 문자 입력이 없을때는 모든 데이터를 보여준다.
+        if (charText.length() == 0) {
+            items.addAll(copy);
+        }
+        // 문자 입력을 할때..
+        else
+        {
+            // 리스트의 모든 데이터를 검색한다.
+            for(int i = 0;i < copy.size(); i++)
+            {
+                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
+                if (copy.get(i).getUser_id().toLowerCase().contains(charText))
+                {
+                    // 검색된 데이터를 리스트에 추가한다.
+                    items.add(copy.get(i));
+                }
+            }
+        }
+        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
+        chatlist_listView.setAdapter(adapter);
     }
 }
