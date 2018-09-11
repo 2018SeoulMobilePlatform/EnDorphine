2839b77871ce27e0c14525a5c405edeb503052da
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2018-09-12 01:25:39 +0900

    feat: 채팅 목록 Tab 구현

--- app/src/main/java/endorphine/icampyou/ExchangeMenu/ChattingList_Fragment.java
+++ app/src/main/java/endorphine/icampyou/ExchangeMenu/ChattingList_Fragment.java
@@ -18,12 +18,9 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-<<<<<<< HEAD:app/src/main/java/endorphine/icampyou/ChattingList_Fragment.java
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
-=======
->>>>>>> 9ae8671cdfc6b5b51839038d0b43f20b76be2da8:app/src/main/java/endorphine/icampyou/ExchangeMenu/ChattingList_Fragment.java
 
 import endorphine.icampyou.BaseFragment;
 import endorphine.icampyou.R;
@@ -47,21 +44,21 @@ public class ChattingList_Fragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_chattinglist,container,false);
 
-//        // 탭 호스트에 탭 추가
-//        TabHost tabHost = (TabHost)view.findViewById(R.id.tabHost_listview);
-//        tabHost.setup();
-//
-//        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
-//        TabHost.TabSpec ts1 = tabHost.newTabSpec("Tab Spec 1");
-//        ts1.setContent(R.id.chatlist1);
-//        ts1.setIndicator("Camp ChatList");
-//        tabHost.addTab(ts1);
-//
-//        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
-//        TabHost.TabSpec ts2 = tabHost.newTabSpec("Tab Spec 2");
-//        ts2.setContent(R.id.chatlist2);
-//        ts2.setIndicator("My ChatList");
-//        tabHost.addTab(ts2);
+        // 탭 호스트에 탭 추가
+        TabHost tabHost1 = (TabHost)view.findViewById(R.id.tapHost_chatlist);
+        tabHost1.setup();
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
+        ts1.setContent(R.id.content1_chatlist);
+        ts1.setIndicator("캠핑장 채팅");
+        tabHost1.addTab(ts1);
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2");
+        ts2.setContent(R.id.content2_chatlist);
+        ts2.setIndicator("나의 채팅");
+        tabHost1.addTab(ts2);
 
         Button add_chatlist_btn = (Button) view.findViewById(R.id.make_chatlist_button);
         add_chatlist_btn.setOnClickListener(new View.OnClickListener()
@@ -74,54 +71,51 @@ public class ChattingList_Fragment extends BaseFragment {
             }
         });
 
-<<<<<<< HEAD:app/src/main/java/endorphine/icampyou/ChattingList_Fragment.java
-
         adapter = new ChatList_Adapter(getActivity());
 
-        listView = (ListView) view.findViewById(R.id.camp_chat_listview);
-=======
-        adapter = new ChatList_Adapter(getActivity());
+        //listView = (ListView) view.findViewById(R.id.camp_chat_listview);
 
-        listView = (ListView) view.findViewById(R.id.chat_listview);
->>>>>>> 9ae8671cdfc6b5b51839038d0b43f20b76be2da8:app/src/main/java/endorphine/icampyou/ExchangeMenu/ChattingList_Fragment.java
-        listView.setAdapter(adapter);
+        adapter.add(null,"전세영","냥냥","냥냥");
 
-        //채팅방 들어가기
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view ,int position,long id){
-                startFragment(getFragmentManager(),ChattingMessage_Fragment.class);
-            }
-        });
+        //listView.setAdapter(adapter);
 
-        //채팅 목록 삭제
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int position, long arg3) {
 
-                DialogInterface.OnClickListener positiveListener = new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        adapter.remove(position);
-                        listView.setAdapter(adapter);
-                    }
-                };
-
-                DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                };
-
-                new AlertDialog.Builder(getActivity())
-                        .setTitle("채팅 목록을 삭제하시겠습니까?")
-                        .setPositiveButton("예",positiveListener)
-                        .setNegativeButton("취소",cancelListener).show();
-
-                return true;
-            }
-        });
+//        //채팅방 들어가기
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view ,int position,long id){
+//                startFragment(getFragmentManager(),ChattingMessage_Fragment.class);
+//            }
+//        });
+//
+//        //채팅 목록 삭제
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int position, long arg3) {
+//
+//                DialogInterface.OnClickListener positiveListener = new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        adapter.remove(position);
+//                        listView.setAdapter(adapter);
+//                    }
+//                };
+//
+//                DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                };
+//
+//                new AlertDialog.Builder(getActivity())
+//                        .setTitle("채팅 목록을 삭제하시겠습니까?")
+//                        .setPositiveButton("예",positiveListener)
+//                        .setNegativeButton("취소",cancelListener).show();
+//
+//                return true;
+//            }
+//        });
 
         editSearch = (EditText)view.findViewById(R.id.search);
         editSearch.addTextChangedListener(new TextWatcher(){
--- app/src/main/java/endorphine/icampyou/MainActivity.java
+++ app/src/main/java/endorphine/icampyou/MainActivity.java
@@ -1,23 +1,25 @@
 package endorphine.icampyou;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentTransaction;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import endorphine.icampyou.ExchangeMenu.ChattingList_Fragment;
-import endorphine.icampyou.Login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    String pass_user;
-    String pass_need;
-    String pass_lettable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ChattingList_Fragment chattingList_fragment = new ChattingList_Fragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.main,chattingList_fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
