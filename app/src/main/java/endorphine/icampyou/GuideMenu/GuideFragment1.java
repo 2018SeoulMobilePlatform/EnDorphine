18a7867ac088cd0e0ff72b82c23872a641b07610
LeeeeDain <dain6912@naver.com>
2018-09-12 15:59:31 +0900

    feat: change drawer navigation bar

--- app/src/main/java/endorphine/icampyou/GuideMenu/GuideFragment1.java
+++ app/src/main/java/endorphine/icampyou/GuideMenu/GuideFragment1.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import endorphine.icampyou.BaseFragment;
 import endorphine.icampyou.R;
@@ -16,10 +17,12 @@ import endorphine.icampyou.R;
  */
 public class GuideFragment1 extends BaseFragment implements View.OnClickListener {
     private View view;
-    private Button nanji;
-    private Button choansan;
-    private Button jungrang;
-    private Button gangdong;
+    private ImageView nanji;
+    private ImageView choansan;
+    private ImageView jungrang;
+    private ImageView gangdong;
+    private ImageView noeul;
+    private ImageView seoul;
 
     // 프래그먼트 xml 설정하는 메소드
     @Override
@@ -28,10 +31,12 @@ public class GuideFragment1 extends BaseFragment implements View.OnClickListener
         view = inflater.inflate(R.layout.fragment_guide_1, container, false);
 
         // 각 xml에 맞는 버튼들 연결해줌
-        nanji = (Button) view.findViewById(R.id.nanji);
-        choansan = (Button) view.findViewById(R.id.choansan);
-        jungrang = (Button) view.findViewById(R.id.jungrang);
-        gangdong = (Button) view.findViewById(R.id.jungrang);
+        nanji = (ImageView) view.findViewById(R.id.nanji);
+        choansan = (ImageView) view.findViewById(R.id.choansan);
+        jungrang = (ImageView) view.findViewById(R.id.jungrang);
+        gangdong = (ImageView) view.findViewById(R.id.gangdong);
+        noeul = (ImageView)view.findViewById(R.id.noeul);
+        seoul = (ImageView)view.findViewById(R.id.seoul);
 
         // 버튼 클릭 메소드 (각 프래그먼트간 유동성 확인하려고 임의로 난지버튼 누르면 예약화면으로 가게함)
         nanji.setOnClickListener(this);
--- app/src/main/java/endorphine/icampyou/HomeActivity.java
+++ app/src/main/java/endorphine/icampyou/HomeActivity.java
@@ -139,19 +139,19 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
