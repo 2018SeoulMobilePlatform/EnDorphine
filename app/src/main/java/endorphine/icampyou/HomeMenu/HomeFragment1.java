2a9867f94429a72e11d88933e89f2d33bcf70ad0
Jiho <giho-p@hanmail.net>
2018-09-18 17:13:29 +0900

    feat: 서울시 API 적용

--- app/src/main/java/endorphine/icampyou/HomeMenu/HomeFragment1.java
+++ app/src/main/java/endorphine/icampyou/HomeMenu/HomeFragment1.java
@@ -1,87 +1,134 @@
 package endorphine.icampyou.HomeMenu;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.drawable.AnimationDrawable;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 import endorphine.icampyou.BaseFragment;
 import endorphine.icampyou.R;
+import kr.go.seoul.airquality.AirQualityTypeMini;
 
 /**
  * 홈 화면 프래그먼트1 클래스
  */
-public class HomeFragment1 extends BaseFragment {
+public class HomeFragment1 extends BaseFragment implements AsyncResponse {
+
+    ReceiveWeatherTask receiveWeatherTask = new ReceiveWeatherTask();
+
+    private AirQualityTypeMini typeMini;
+    private String OpenApiKey = "7a66636d476769683532654d436a63";
     private View view;
+    public String description="";
+    public String temperature="";
+    public String temp="";
 
-    // 프래그먼트 xml 설정하는 메소드
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.fragment_home_1);
-//    }
+    public static boolean mAsync = false;
 
+    ImageView weatherIcon;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_1, container, false);
-        ImageView imgView = (ImageView) view.findViewById(R.id.animationImage);
-        imgView.setVisibility(ImageView.VISIBLE);
-        imgView.setBackgroundResource(R.drawable.simple_animation);
-        AnimationDrawable frameAnimation = (AnimationDrawable) imgView.getBackground();
-        //getWeatherData();
-        frameAnimation.start();
+//        ImageView imgView = (ImageView) view.findViewById(R.id.animationImage);
+//        imgView.setVisibility(ImageView.VISIBLE);
 
-        Log.e("HomeFragment1", "1");
+        typeMini = (AirQualityTypeMini) view.findViewById(R.id.button_mini);
+        typeMini.setOpenAPIKey(OpenApiKey);
+
+        receiveWeatherTask.delegate = this;
+
+        if(mAsync==false) {
+            mAsync = true;
+            String url = "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=9595c31a22a4095ba487c9b0f1e062bc";
+            receiveWeatherTask.execute(url);
+        }
+        TextView weatherDescription = (TextView) view.findViewById(R.id.weather_description);
+        weatherDescription.setText(description);
+        Log.e("날씨", description);
+        TextView currentTemperature = (TextView) view.findViewById(R.id.current_temperature);
+        currentTemperature.setText(temperature);
+        Log.e("온도", temperature);
+        weatherIcon = (ImageView) view.findViewById(R.id.weather_icon);
+
+        setWeatherIcon(description);
 
         return view;
     }
 
-    public void getWeatherData() {
+    @Override
+    public void descriptionFinish(String output) {
 
-        String url = "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=9595c31a22a4095ba487c9b0f1e062bc";
-
-        ReceiveWeatherTask receiveWeatherTask = new ReceiveWeatherTask();
-        receiveWeatherTask.execute(url);
-
-
-
-//        BufferedReader br = null;
-//        try{
-//            String urlstr = "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=9595c31a22a4095ba487c9b0f1e062bc";
-//            Log.e("HomeFragment1", "9");
-//            URL url = new URL(urlstr);
-//            Log.e("HomeFragment1", "8");
-//            HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
-//            Log.e("HomeFragment1", "7");
-//            urlconnection.setRequestMethod("GET");
-//            Log.e("HomeFragment1", "6");
-//            br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8"));
-//            Log.e("HomeFragment1", "5");
-//            String result = "";
-//            String line;
-//            Log.e("HomeFragment1", "4");
-//            while((line = br.readLine()) != null) {
-//                result = result + line + "\n";
-//            }
-//            Log.e("HomeFragment1" + "1", result);
-//            System.out.println(result);
-//        } catch(Exception e) {
-//            Log.e("HomeFragment1", "2");
-//            System.out.println(e.getMessage());
-//        }
-
+        description = output;
+        TextView textView = (TextView) getView().findViewById(R.id.weather_description);
+        setWeatherIcon(description);
+        textView.setText(description);
     }
 
+    @Override
+    public void temperatureFinish(String output){
+
+        temperature = output + "°C";
+        TextView textView = (TextView) getView().findViewById(R.id.current_temperature);
+        textView.setText(temperature);
+    }
+
+    public void setWeatherIcon(String weather){
+
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        Log.e("hour", Integer.toString(hour));
+        //ImageView weatherIcon = (ImageView) getView().findViewById(R.id.weather_icon);
+
+        switch(weather) {
+
+            case "맑음":
+                if (hour >= 18 || hour < 6)
+                    weatherIcon.setBackgroundResource(R.drawable.moon);
+                else
+                    weatherIcon.setBackgroundResource(R.drawable.sunny);
+                break;
+
+            case "구름 조금":
+                if (hour >= 18 || hour < 6)
+                    weatherIcon.setBackgroundResource(R.drawable.night);
+                else
+                    weatherIcon.setBackgroundResource(R.drawable.sun);
+                break;
+
+            case "흐림":
+                weatherIcon.setBackgroundResource(R.drawable.clouds);
+                break;
+
+            case "비 조금":
+                weatherIcon.setBackgroundResource(R.drawable.drizzle);
+                break;
+
+            case "비":
+                weatherIcon.setBackgroundResource(R.drawable.rain);
+                break;
+
+            case "천둥번개":
+                weatherIcon.setBackgroundResource(R.drawable.storm);
+                break;
+
+            case "눈":
+                weatherIcon.setBackgroundResource(R.drawable.snowman);
+                break;
+
+            case "안개":
+                weatherIcon.setBackgroundResource(R.drawable.fog);
+                break;
+
+            default:
+                weatherIcon.setBackgroundResource(R.drawable.sunny);
+                break;
+        }
+    }
 }
